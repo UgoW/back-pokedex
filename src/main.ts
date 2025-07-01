3992db018c8f98c269de377a82997f42a3dc8f4f
maximedervaux <maximeder.pro@gmail.com>
2025-07-01 22:15:58 +0200

    fix: :bug: remove hardcoded CORS origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', 
     credentials: true,              
   });
 
